9d5603f8b207766e221a17b212a445557bc5675e
mnmzc <100777450+mnmzc@users.noreply.github.com>
2023-02-28 13:31:19 -0500

    change image sources to use the dmp content delivery network, other files will use raw github user content file pages

--- static/main.js
+++ static/main.js
@@ -77,7 +77,7 @@ function createWindow(assignment) {
   var media = assignment.medias[0];
 
   request(
-    "http://dmp-service.tk/edware/main_header.html",
+    "https://raw.githubusercontent.com/mnmzc/edware/master/static/main_header.html",
     (res) => {
       var createdDate = new Date(media.createdAt);
       var updatedDate = new Date(media.updatedAt);
@@ -189,7 +189,7 @@ function parseQuestions(questions) {
     element_id("questions-column").innerHTML += toWrite;
   });
 
-  request("http://dmp-service.tk/edware/main_footer.html", (res2) => {
+  request("https://raw.githubusercontent.com/mnmzc/edware/master/static/main_footer.html", (res2) => {
     authWndw.document.write(res2);
   });
 }
